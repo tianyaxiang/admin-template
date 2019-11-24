87b03cbb7fb117f44eacaad9e1fbe619fe151fa2
tianyaxiang <tianyaxiang@qq.com>
2019-11-24 12:46:28 +0800

    add login

--- springboot-adminlte/src/main/java/com/tianyaxiang/springboot/adminlte/SpringbootAdminlteApplication.java
+++ springboot-adminlte/src/main/java/com/tianyaxiang/springboot/adminlte/SpringbootAdminlteApplication.java
@@ -3,13 +3,29 @@ package com.tianyaxiang.springboot.adminlte;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @author tianyaxiang
  */
 @SpringBootApplication
 public class SpringbootAdminlteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootAdminlteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAdminlteApplication.class, args);
+
+        List<String> stockList = new ArrayList<String>();
+        stockList.add("stock1");
+        stockList.add("stock2");
+        stockList.add("stock3");
+
+        for (Iterator<String> i = stockList.iterator(); i.hasNext();) {
+            stockList.remove(2);
+            String item = i.next();
+            System.out.println(item);
+        }
+
+    }
 }
